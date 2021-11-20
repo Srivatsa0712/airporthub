d3aac20e1a2c8e26a59fe79eab9047a5985c396a
Srivatsa Sugurkar <79782950+Srivatsa0712@users.noreply.github.com>
2021-11-20 09:41:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function welcome(){
+    alert("Welcome to Chatrapati Shivaji Maharaj International Airport");
+}
+    
+function airIndia(){
+    alert("Welcome to AirIndia✈︎");
+    alert("Your Flight from Mumbai to Hyderabad is delayed by 5:00 Hours");
+}
+    
+function emirates(){
+    alert("Welcome to Emirates!!!");
+    alert("Your Flight from Mumbai to Dubai will departure shortly.Pls board the Plane as soon as possible");
+}
+    
+function spicejet(){
+    alert("Welcome to Spicejet!!!");
+    alert("Your Flight from Mumbai to Delhi is on time.");
+}
+    
+function indigo(){
+    alert("Welcome to Indigo!!!");
+    alert("Flight from Mumbai to Chennai has been cancelled due to bad weather.")
+}
